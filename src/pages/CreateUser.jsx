7b0cecef01f18d42c703b18f001fe9eefe39cd46
kilian <kilianvanembden@gmail.com>
2021-06-09 15:46:41 +0200

    Bug fix create user

--- src/pages/CreateUser.jsx
+++ src/pages/CreateUser.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../css/main.css';
 import '../css/create-user.css';
 
 import { Link } from 'react-router-dom';
@@ -7,32 +8,21 @@ import { getUserInfo } from '../utils';
 
 class CreateUser extends Component{
 
-  const [UsernameReg, setUsernameReg] = useState("")
-  const [PasswordReg, setPasswordReg] = useState("")
-
   render (){
     return(
-      <main>
-        <div className="CreateUser">
+      <main className="">
+        <div className="create-user">
           <h1>Registration</h1>
-          <label>Ussername<label>
+          <label>Username</label>
           <input type="text"
-          placeholder="Work Email"
-          onChange={(e) => {
-            setUsernameReg(e.target.value)
-            }}
-          />
+          placeholder="Work Email"/>
           <label>Password</label>
-          <input type="text" placeholder="Password.."
-          onChange={(e) => {
-            setPasswordReg(e.target.value)
-            }}
-          />
+          <input type="text" placeholder="Password.."/>
           <label>Confirm Password</label>
           <input type="text" placeholder="Confirm Password.."/>
           <button>Register</button>
         </div>
-        <div className="CreateUserNotes">
+        <div className="create-user__notes">
         </div>
       </main>
     );

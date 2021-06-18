f68dc0fb3b1e3cbcb1de09fc86683ba39cc64932
Kilian van Embden <kilianvanembden@gmail.com>
2021-06-18 12:11:54 +0200

    Create user rol toegevoegd2

--- src/pages/CreateUser.jsx
+++ src/pages/CreateUser.jsx
@@ -19,19 +19,6 @@ class CreateUser extends Component{
   render (){
     return(
       <main className="create-user">
-        <div className="create-user--form">
-          <h1>Registration</h1>
-          <label>Username</label>
-          <input type="text"
-          placeholder="Work Email"/>
-          <label>Password</label>
-          <input type="text" placeholder="Password.."/>
-          <label>Confirm Password</label>
-          <input type="text" placeholder="Confirm Password.."/>
-          <button>Register</button>
-        </div>
-        <div className="create-user__notes">
-        </div>
         <h1>Registration</h1>
         <form action="LEEG" className="create-user--form">
           <div className="create-user--form--group">

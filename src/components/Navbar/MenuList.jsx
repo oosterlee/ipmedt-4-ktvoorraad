9614da4d0d77a44b35ff0070f08391c3fe5a94f6
Davino Rosaria <doubled.rosaria@gmail.com>
2021-06-21 11:03:56 +0200

    Navbar pre localstorage fix.

--- src/components/Navbar/MenuList.jsx
+++ src/components/Navbar/MenuList.jsx
@@ -1,14 +1,58 @@
+const login = localStorage.getItem("login");
+
+let callbackMenuItems = [];
+
 export const MenuList = [
   {
-    title: "Products",
+    title: "Producten",
     url: "/products",
   },
   {
-    title: "Cart",
+    title: "Winkelwagen",
     url: "/cart",
   },
+
   {
     title: "Login",
     url: "/login",
   },
 ];
+
+export const LoggedInMenu = [
+  {
+    title: "Producten",
+    url: "/products",
+  },
+  {
+    title: "Winkelwagen",
+    url: "/cart",
+  },
+
+  {
+    title: "Profiel",
+    url: "/profile",
+  },
+];
+
+export const onMenuChange = (func) => {
+  console.log("Dut ia wwn");
+  callbackMenuItems.push(func);
+};
+
+function callCallbacks() {
+  // hier voeg je items toe in de array
+  for (let i = 0; i < callbackMenuItems.length; i++) {
+    callbackMenuItems[i](LoggedInMenu);
+  }
+}
+
+// setTimeout(
+//   () => {console.log("NAv timeout ")}, 2000
+// );
+setTimeout(
+  // als je bent ingelooged moet je deze functie aanroepen
+  () => {
+    callCallbacks();
+  },
+  2000
+);

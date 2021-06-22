dc7c4f369e0ef9bfb9d5ea4c3ab8fba2ad16b945
Davino Rosaria <doubled.rosaria@gmail.com>
2021-06-22 16:52:11 +0200

    changed menu list

--- src/components/Navbar/MenuList.jsx
+++ src/components/Navbar/MenuList.jsx
@@ -27,6 +27,77 @@ export const LoggedInMenu = [
   },
 
   {
+    title: "Bestelgeschiedenis",
+    url: "/orderhistory/:id",
+  },
+
+  {
+    title: "Profiel",
+    url: "/profile",
+  },
+
+  
+];
+
+export const LoggedInManager = [
+  {
+    title: "Producten",
+    url: "/products",
+  },
+  {
+    title: "Winkelwagen",
+    url: "/cart",
+  },
+
+  {
+    title: "Bestelgeschiedenis",
+    url: "/orderhistory/:id",
+  },
+
+  {
+    title: "Bestelverzoeken",
+    url: "/management/orderrequests",
+  },
+
+  {
+    title: "Profiel",
+    url: "/profile",
+  },
+];
+
+
+export const LoggedInAdmin = [
+  {
+    title: "Producten",
+    url: "/products",
+  },
+  {
+    title: "Winkelwagen",
+    url: "/cart",
+  },
+
+  {
+    title: "Bestelgeschiedenis",
+    url: "/orderhistory/:id",
+  },
+
+  {
+    title: "Product Toevoegen",
+    url: "/toevoegen",
+  },
+
+
+  {
+    title: "Bestelverzoeken",
+    url: "/management/orderrequests",
+  },
+
+  {
+    title: "Excel-export",
+    url: "/management/exports",
+  },
+
+  {
     title: "Profiel",
     url: "/profile",
   },

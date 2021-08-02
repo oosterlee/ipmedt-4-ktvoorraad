7dd289ab483380c56a2cdfbb86abdd07d3d361ba
Kasper <s1118202@student.hsleiden.nl>
2021-08-02 14:45:09 +0200

    profiel kan nu bewerkt worden

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -10,6 +10,7 @@ class Profile extends Component {
 
 		this.state = {
 			new_password: "",
+            id: "",
             name: "",
             email: "",
             address: "",
@@ -17,13 +18,16 @@ class Profile extends Component {
             postalcode: "",
             get_user_done: false,
             logged_out: false,
+            profile__wrapper: "block",
+            profile__form: "none",
 		}
 	}
 
     getUser() {
         if(this.state.get_user_done == false){
             apiClient.get('/api/user').then(response => {
-                this.setState({name: response.data.name,
+                this.setState({id: response.data.id,
+                                name: response.data.name,
                                 email: response.data.email,
                                 address: response.data.address,
                                 housenumber: response.data.housenumber,
@@ -33,6 +37,26 @@ class Profile extends Component {
                 // this.setState({logged_out: true});
             });
         }
+    }
+
+    show_profile_edit(e){
+        e.preventDefault();
+        this.setState({profile__wrapper: "none", profile__form: "flex"});
+    }
+
+    profile_edit(event){
+        event.preventDefault(); //voorkomt dat de pagina refreshed
+        apiClient.put("/api/user/update",
+        {
+            id: this.state.id,
+            name: this.state.name,
+            email: this.state.email,
+            address: this.state.address,
+            housenumber: this.state.housenumber,
+            postalcode: this.state.postalcode,   
+        }).then(response => {
+            this.setState({profile__wrapper: "block", profile__form: "none"});
+        });
     }
 
     logout(){
@@ -53,14 +77,44 @@ class Profile extends Component {
 
 		return (
             <article className="profile">
-                <div className="profile__wrapper">
+                <div style={{display: this.state.profile__wrapper, gridColumnStart: 2}}>
                     <p className="profile__info">Naam: {this.state.name}</p>
                     <p className="profile__info">Email: {this.state.email}</p>
                     <p className="profile__info">Adres: {this.state.address}</p>
                     <p className="profile__info">Huis nummer: {this.state.housenumber}</p>
                     <p className="profile__info">Postcode: {this.state.postalcode}</p>
                     <button className="profile__logout" onClick={this.logout.bind(this)}>Uitloggen</button>
+                    <a className="profile_bewerken" href="#" onClick={e => this.show_profile_edit(e)}>Profiel bewerken</a>
                 </div>
+                <form  className="profile__form" style={{display: this.state.profile__form, gridColumnStart: 2, padding: "2rem", alignItems: "center", flexDirection: "column"}} onSubmit={event => this.profile_edit(event)}>
+                    <div className="profile__form__group">
+						<label className="profile__form__label">Naam</label>
+						<input className="profile__form__input" name="name" type="text" placeholder="Naam" required value={this.state.name} onChange={e => this.setState({ name: e.target.value })}/>
+					</div>
+
+                    <div className="profile__form__group">
+						<label className="profile__form__label">Email</label>
+						<input className="profile__form__input" name="email" type="email" placeholder="Email" required value={this.state.email} onChange={e => this.setState({ email: e.target.value })}/>
+					</div>
+
+                    <div className="profile__form__group">
+						<label className="profile__form__label">Adres</label>
+						<input className="profile__form__input" name="address" type="text" placeholder="Adres" required value={this.state.address} onChange={e => this.setState({ address: e.target.value })}/>
+					</div>
+
+                    <div className="profile__form__group">
+						<label className="profile__form__label">Huis nummer</label>
+						<input className="profile__form__input" name="housenumber" type="text" placeholder="Huis nummer" required value={this.state.housenumber} onChange={e => this.setState({ housenumber: e.target.value })}/>
+					</div>
+
+                    <div className="profile__form__group">
+						<label className="profile__form__label">Postcode</label>
+						<input className="profile__form__input" name="postalcode" type="text" placeholder="Postcode" required value={this.state.postalcode} onChange={e => this.setState({ postalcode: e.target.value })}/>
+					</div>
+
+                    <button className="profile__form__button" type="submit">Profiel Aanpassen</button>
+
+                </form>
             </article>
 		);
 	}

fbf7c0842e1c9b159598782df375f611b2658d53
Davino Rosaria <doubled.rosaria@gmail.com>
2021-08-27 12:52:22 +0200

    merge with dev

--- src/pages/productsCopy.jsx
+++ src/pages/productsCopy.jsx
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
 import '../css/products.css';
 import axios from "axios";
-
 import { Link } from 'react-router-dom';
-
 import ProductsItem from '../components/ProductsItem';
 import Searchbar from "../components/Searchbar";
-import Loading from "../components/Loading";
-import Category from '../components/Category';
-
 
 class Products extends Component {
 	constructor(props) {
@@ -18,14 +13,11 @@ class Products extends Component {
 			products: [],
 			searchValue: "",
 			renderProducts: [],
-			packs: [],
-			renderPacks: [],
-			loading: true,
 			list: false,
 		};
 	}
 
-	componentDidMount(){
+	componentDidMount() {
 		this.makeApiCall();
 	}
 
@@ -36,43 +28,40 @@ class Products extends Component {
 
 	makeApiCall = searchTerm => {
 		const BASE_URL = "http://localhost:8000/api/products";
-		axios.get((process.env.REACT_APP_BASE_URL || 'http://127.0.0.1:8000') + '/api/products').then(json => this.setState({ products: json.data, renderProducts: json.data, loading: false }));
-		axios.get((process.env.REACT_APP_BASE_URL || 'http://127.0.0.1:8000') + '/api/pack').then(json => this.setState({ packs: json.data, renderPacks: json.data, loading: false }));
+		axios.get((process.env.REACT_APP_BASE_URL || 'http://127.0.0.1:8000') + '/api/products').then(json => this.setState({ products: json.data, renderProducts: json.data }));
 	}
 
-	search(e){
+	search(e) {
 		let searchValue = e.target.value;
 		let products = this.state.products;
 		let renderProducts = []
 		products.forEach((item, i) => {
-            if (JSON.stringify(item).toLowerCase().includes(searchValue.toLowerCase())) renderProducts.push(item);
-        });
+			if (JSON.stringify(item).toLowerCase().includes(searchValue.toLowerCase())) renderProducts.push(item);
+		});
 		console.log(e);
-		this.setState({searchValue, renderProducts});
+		this.setState({ searchValue, renderProducts });
 	}
 
 
 
 	render() {
+
 		let layout__class = this.state.list ? "products__grid" : "products__list";
-		if (this.state.loading) return(<Loading />);
+
 		return (
+			
 			<section className="products">
-				<Searchbar value={this.state.searchValue} onChange={this.search.bind(this)}/>
-				<Category/>
+				<Searchbar value={this.state.searchValue} onChange={this.search.bind(this)} />
+				{/* list to grid buttons */}
 				<div>
 					<button classname="toggleButton"onClick={(e) => this.changeLayout(e)}>
 						Toggle Layout
 					</button>
             	</div>
-				<ul className={layout__class}>
+				
+				<ul  className={layout__class}>
 					{
-						this.state.renderPacks.map((item, i) => 
-							<ProductsItem {...item} key={item.id} isPack={true} />
-						)
-					}
-					{
-						this.state.renderProducts.map((item, i) => 
+						this.state.renderProducts.map((item, i) =>
 							<ProductsItem {...item} key={item.id} />
 						)
 					}
@@ -81,7 +70,6 @@ class Products extends Component {
 			</section>
 		);
 	}
-
 }
 
 export default Products;
